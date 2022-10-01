74f72feabcb8d94a7d0e71274fef87612b2e2e2b
Juarez Martins <juniorbmx13@gmail.com>
2022-09-30 22:06:26 -0300

    feat: exe investigação arrays

--- InvestigacaoArrays/src/Programa.java
+++ InvestigacaoArrays/src/Programa.java
@@ -1,2 +1,35 @@
+/*
+Utilizando listas, faça um programa que faça 5 perguntas para uma
+pessoa sobre um crime. As perguntas são:
+"Telefonou para a vítima?"
+"Esteve no local do crime?"
+"Mora perto da vítima?"
+"Devia para a vítima?"
+"Já trabalhou com a vítima?"
+Se a pessoa responder positivamente a 2 questões ela deve ser
+classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como
+"Assassina". Caso contrário, ela será classificado como "Inocente".
+Exercício para pŕatica Java Collections - List.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Programa {
+    public static void main (String [] args){
+
+        Scanner scan = new Scanner(System.in);
+        List<Boolean> lista = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Telefonou para a vítima?");
+
+            System.out.println("Esteve no local do crime?");
+            System.out.println("Mora perto da vítima?");
+            System.out.println("Devia para a vítima?");
+            System.out.println("Já trabalhou com a vítima?");
+        }
+
+    }
 }
